2cc762085cb6048276b1788bd60416e7fb028df7
Daniel De La Fuentes <d33fu3nt35@gmail.com>
2017-06-19 15:50:10 -0500

    Created Post Class With Private Properties & Getters & Setters for Title & Body. Also added 2 HTML pages underneath posts subdirectory in template directory.

---
+++ src/main/java/com/example/demo/Post.java
@@ -0,0 +1,26 @@
+package com.example.demo;
+
+/**
+ * Created by daniel on 6/19/17.
+ */
+public class Post {
+
+    private String title;
+    private String body;
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getBody() {
+        return body;
+    }
+
+    public void setBody(String body) {
+        this.body = body;
+    }
+}
